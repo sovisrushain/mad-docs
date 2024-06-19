b75c82d2eb230871469d8d22f81fbc9ebb438456
sovisrushain <sovisrushain@gmail.com>
2024-06-20 00:27:43 +0530

    add bean validations

--- hospital-service/src/main/java/com/cisco/hospital_service/model/Hospital.java
+++ hospital-service/src/main/java/com/cisco/hospital_service/model/Hospital.java
@@ -1,5 +1,8 @@
 package com.cisco.hospital_service.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +15,17 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Hospital implements Serializable {
+
+    @NotNull(message = "Hospital ID can not be null")
+    @Pattern(regexp = "^H\\d{3}$", message = "Wrong hospital ID format")
     private String hospitalId;
+
+    @NotNull(message = "Hospital name can not be null")
+    @NotBlank(message = "Hospital name cannot be blank")
     private String hospitalName;
+
+    @NotNull(message = "Hospital address can not be null")
+    @NotBlank(message = "Hospital address cannot be blank")
     private String hospitalAddress;
+
 }
