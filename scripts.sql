2426822db64225cfe69e52fa82e538f7cdde0ad9
sovisrushain <sovisrushain@gmail.com>
2024-06-22 22:10:56 +0530

    db scripts

---
+++ scripts.sql
@@ -0,0 +1,7 @@
+-- hospital
+
+CREATE TABLE Hospital (
+                          hospitalId VARCHAR(255) NOT NULL PRIMARY KEY,
+                          hospitalName VARCHAR(255) NOT NULL,
+                          hospitalAddress VARCHAR(255) NOT NULL
+);
