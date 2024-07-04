a2a94a318afd9bfa576167905b6e7ac1781b0a2d
sovisrushain <sovisrushain@gmail.com>
2024-07-04 23:41:32 +0530

    create doctor schedule repo

---
+++ doctor-schedule-service/src/main/java/com/cisco/doctor_schedule_service/repository/DoctorScheduleRepository.java
@@ -0,0 +1,4 @@
+package com.cisco.doctor_schedule_service.repository;
+
+public interface DoctorScheduleRepository {
+}
---
+++ doctor-schedule-service/src/main/java/com/cisco/doctor_schedule_service/repository/impl/DoctorScheduleRepositoryImpl.java
@@ -0,0 +1,8 @@
+package com.cisco.doctor_schedule_service.repository.impl;
+
+import com.cisco.doctor_schedule_service.repository.DoctorScheduleRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DoctorScheduleRepositoryImpl implements DoctorScheduleRepository {
+}
