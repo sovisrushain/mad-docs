65b05d9c261b4a1408f77f36fdfd4257f4a43986
sovisrushain <sovisrushain@gmail.com>
2024-07-12 22:07:41 +0530

    unit tests

--- doctor-service/src/test/java/com/cisco/doctor_service/controller/DoctorControllerTest.java
+++ doctor-service/src/test/java/com/cisco/doctor_service/controller/DoctorControllerTest.java
@@ -1,26 +1,65 @@
 package com.cisco.doctor_service.controller;
 
+import com.cisco.doctor_service.dto.ResponseDTO;
+import com.cisco.doctor_service.model.Doctor;
 import com.cisco.doctor_service.service.DoctorService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
 class DoctorControllerTest {
 
-    @Mock
-    private DoctorService doctorService;
-
     @InjectMocks
     private DoctorController doctorController;
 
+    @Mock
+    private DoctorService doctorService;
+
+    @Mock
+    private BindingResult bindingResult;
+
     @BeforeEach
-    void setUp() {
-        doctorController = new DoctorController(doctorService);
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
+
+//    @Test
+//    public void test_Get_All_Doctors() {
+//        List<Doctor> doctors = Arrays.asList(new Doctor(), new Doctor());
+//        when(doctorService.getAllDoctors()).thenReturn(doctors);
+//
+//        ResponseDTO<List<Doctor>> response = doctorController.getAllDoctors();
+//
+//        assertEquals(HttpStatus.OK, response.getStatusCode());
+//        assertEquals(doctors, response.getData());
+//        assertEquals("all doctors", response.getMessage());
+//    }
+//
+//    @Test
+//    public void test_Get_All_Doctors_Empty() {
+//        when(doctorService.getAllDoctors()).thenReturn(new ArrayList<>());
+//
+//        ResponseDTO<List<Doctor>> response = doctorController.getAllDoctors();
+//
+//        assertEquals(HttpStatus.OK, response.getStatusCode());
+//        assertEquals(0, response.getData().size());
+//        assertEquals("there are no doctors registered", response.getMessage());
+//    }
 
 
 }
