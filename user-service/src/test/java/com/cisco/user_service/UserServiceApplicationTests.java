c7fdc4e8409a1a982409222785c0471e85477ff4
sovisrushain <sovisrushain@gmail.com>
2024-08-05 21:33:59 +0530

    initialize user service

---
+++ user-service/src/test/java/com/cisco/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cisco.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
