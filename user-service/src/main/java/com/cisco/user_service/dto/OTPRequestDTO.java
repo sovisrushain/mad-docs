752f4a61b3bf8316f34cb5fef593b941fad58f18
sovisrushain <sovisrushain@gmail.com>
2024-08-05 22:23:08 +0530

    create otp dto

---
+++ user-service/src/main/java/com/cisco/user_service/dto/OTPRequestDTO.java
@@ -0,0 +1,15 @@
+package com.cisco.user_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OTPRequestDTO {
+    private String mobileNo;
+    private String sender;
+}
