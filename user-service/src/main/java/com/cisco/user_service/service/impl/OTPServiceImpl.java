327da9b87f32184c996d3fbeda1c2d53ddaf7393
sovisrushain <sovisrushain@gmail.com>
2024-08-06 21:09:37 +0530

    generate otp

--- user-service/src/main/java/com/cisco/user_service/service/impl/OTPServiceImpl.java
+++ user-service/src/main/java/com/cisco/user_service/service/impl/OTPServiceImpl.java
@@ -4,15 +4,25 @@ import com.cisco.user_service.dto.OTPRequestDTO;
 import com.cisco.user_service.service.OTPService;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+
 @Service
 public class OTPServiceImpl implements OTPService {
     @Override
     public String createOTP(OTPRequestDTO otpRequestDTO) {
+        String otp = generateOTP();
+        // save otp in backend
         return "";
     }
 
     @Override
     public String validateOTP(OTPRequestDTO otpRequestDTO) {
         return "";
+    }
+
+    private String generateOTP() {
+        SecureRandom random = new SecureRandom();
+        int otp = 100000 + random.nextInt(900000);
+        return String.valueOf(otp);
     }
 }
