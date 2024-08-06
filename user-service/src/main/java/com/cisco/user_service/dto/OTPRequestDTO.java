65b4205a0c9e4b80a6bc707b4286bcf5053aecbe
sovisrushain <sovisrushain@gmail.com>
2024-08-06 21:44:17 +0530

    update otp request dto

--- user-service/src/main/java/com/cisco/user_service/dto/OTPRequestDTO.java
+++ user-service/src/main/java/com/cisco/user_service/dto/OTPRequestDTO.java
@@ -10,6 +10,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OTPRequestDTO {
+    private String userId;
     private String mobileNo;
     private String sender;
 }
