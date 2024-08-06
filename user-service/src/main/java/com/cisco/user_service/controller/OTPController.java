5801e2eef7523fda9a06e5dab649a126d6976f94
sovisrushain <sovisrushain@gmail.com>
2024-08-06 20:46:09 +0530

    refactor otp controller

--- user-service/src/main/java/com/cisco/user_service/controller/OTPController.java
+++ user-service/src/main/java/com/cisco/user_service/controller/OTPController.java
@@ -1,22 +1,27 @@
 package com.cisco.user_service.controller;
 
 import com.cisco.user_service.dto.OTPRequestDTO;
+import com.cisco.user_service.service.OTPService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/users")
-public class UserController {
+@RequestMapping("/api/v1/otp")
+@RequiredArgsConstructor
+public class OTPController {
 
-    @PostMapping("/create-otp")
+    private final OTPService otpService;
+
+    @PostMapping("/create")
     public String createOTP(@RequestBody OTPRequestDTO otpRequestDTO) {
-        return null;
+        return otpService.createOTP(otpRequestDTO);
     }
 
-    @PostMapping("validate-otp")
+    @PostMapping("/validate")
     public String validateOTP(@RequestBody OTPRequestDTO otpRequestDTO) {
-        return null;
+        return otpService.validateOTP(otpRequestDTO);
     }
 }
